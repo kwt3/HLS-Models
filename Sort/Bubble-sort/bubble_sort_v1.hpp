870f9b64682b18a44fbe0b390411e665b14d604b
kwt3 <72283423+kwt3@users.noreply.github.com>
2024-02-12 08:22:22 -0500

    Update bubble_sort_v1.hpp

--- Sort/Bubble-sort/bubble_sort_v1.hpp
+++ Sort/Bubble-sort/bubble_sort_v1.hpp
@@ -7,16 +7,3 @@ void bubble_sort(ap_uint<32> input_arr[10], ap_uint<32> output_arr[10]);
 
 #endif // BUBBLE_SORT_HPP
 
-
-// Cannot use std::vector due to limitations of HLS compiler
-/*
-#ifndef BUBBLE_SORT_HPP
-#define BUBBLE_SORT_HPP
-
-#include <vector>
-#include <ap_int.h>
-
-void bubble_sort(const std::vector<ap_uint<32>>& input_arr, std::vector<ap_uint<32>>& output_arr);
-
-#endif // BUBBLE_SORT_HPP
-*/
