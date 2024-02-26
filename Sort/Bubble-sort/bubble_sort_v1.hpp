116be40320091b8d55af25cb302b0d860796680b
Trudy Kwok <72283423+kwt3@users.noreply.github.com>
2024-02-26 09:38:38 -0500

    Update bubble_sort_v1.hpp

--- Sort/Bubble-sort/bubble_sort_v1.hpp
+++ Sort/Bubble-sort/bubble_sort_v1.hpp
@@ -1,9 +1,9 @@
-#ifndef BUBBLE_SORT_HPP
-#define BUBBLE_SORT_HPP
+#ifndef BUBBLE_SORT_V1_HPP
+#define BUBBLE_SORT_V1_HPP
 
-#include <ap_int.h>
+typedef int data_t; 
+#define input_size 10 // Pre-define input size
 
-void bubble_sort(data_t input_arr[input_size]);
+void bubble_sort(data_t input_arr[input_size], int size); // Correct function signature
 
-#endif // BUBBLE_SORT_HPP
-
+#endif // BUBBLE_SORT_V1_HPP
