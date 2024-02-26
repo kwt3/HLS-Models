52244decf93a42b315c822c2788f02868298785c
Trudy Kwok <72283423+kwt3@users.noreply.github.com>
2024-02-26 09:40:11 -0500

    Update bubble_sort.cpp

--- Sort/Bubble-sort/bubble_sort.cpp
+++ Sort/Bubble-sort/bubble_sort.cpp
@@ -1,14 +1,12 @@
 #include "bubble_sort_v1.hpp"
 
-
-void bubble_sort(data_t input_arr[input_size])
-{
+void bubble_sort(data_t input_arr[input_size], int size) { // Correct function signature
     // Perform bubble sort on the output array
-    for(int i = 0; i < input_size; i++) {
-        for(int j = 0; j < input_size - i; j++) {
+    for(int i = 0; i < size; i++) { // Use 'size' instead of 'input_size'
+        for(int j = 0; j < size - i - 1; j++) {
             if(input_arr[j] > input_arr[j + 1]) {
-                // Swap output_arr[j] and output_arr[j + 1]
-                ap_uint<32> temp = input_arr[j];
+                // Swap input_arr[j] and input_arr[j + 1]
+                data_t temp = input_arr[j];
                 input_arr[j] = input_arr[j + 1];
                 input_arr[j + 1] = temp;
             }
