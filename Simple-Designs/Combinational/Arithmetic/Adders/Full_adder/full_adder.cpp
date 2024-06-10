805f3b954c7738f30da929dcaf8bb315ef3eb648
Trudy Kwok <kwokt3@gmail.com>
2024-06-10 09:00:41 -0400

    Moved Bubble-sort folder.

--- Simple-Designs/Combinational/Arithmetic/Adders/Full_adder/full_adder.cpp
+++ Simple-Designs/Combinational/Arithmetic/Adders/Full_adder/full_adder.cpp
@@ -15,7 +15,7 @@ void full_adder(T A, T B, T C_In, T& sum, T& carry) {
 }
 
 // Explicit instantiations
-//template void full_adder<bool>(bool A, bool B, bool C_In, bool& sum, bool& carry);
-//template void full_adder<int>(int A, int B, int C_In, int& sum, int& carry);
+// template void full_adder<bool>(bool A, bool B, bool C_In, bool& sum, bool& carry);
+template void full_adder<int>(int A, int B, int C_In, int& sum, int& carry);
 //template void full_adder<ap_int<8>>(ap_int<8> A, ap_int<8> B, ap_int<8> C_In, ap_int<8>& sum, ap_int<8>& carry);
-template void full_adder<ap_uint<8>>(ap_uint<8> A, ap_uint<8> B, ap_uint<8> C_In, ap_uint<8>& sum, ap_uint<8>& carry);
+//template void full_adder<ap_uint<8>>(ap_uint<8> A, ap_uint<8> B, ap_uint<8> C_In, ap_uint<8>& sum, ap_uint<8>& carry);
